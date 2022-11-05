686eb94a5c7bc49b4e5561d2691b216fd1566064
Maksim Okunev <max.vic.okunev@gmail.com>
2022-11-06 00:34:23 +0300

    add positive test

--- src/test/java/CardDeliveryServiceTest.java
+++ src/test/java/CardDeliveryServiceTest.java
@@ -1,20 +1,39 @@
+import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class CardDeliveryServiceTest {
 
 
-    @Test
-    void inputPositive() {
-
-        open("http://localhost:9999/");
-
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
+    String planningDate = generateDate(3);
 
 
+    @Test
+    void inputPositive() {
+       // Configuration.holdBrowserOpen = true;
 
+        open("http://localhost:9999/");
+        $x("//span[@data-test-id='city']//input").val("Самара");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT,Keys.HOME),Keys.BACK_SPACE);
+        $x("//span[@data-test-id='date']//input").val(planningDate);
+        $x("//span[@data-test-id='name']//input").val("Иван Иванов");
+        $x("//span[@data-test-id='phone']//input").val("+79998887744");
+        $x("//label[@data-test-id='agreement']").click();
+        $x("//*[contains(text(), 'Забронировать')]").click();
+        $x("//div[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+
+    }
 
 
 
