8c18cdd6915b28bd31b227c87c14b30a1509714f
Maksim Okunev <max.vic.okunev@gmail.com>
2022-11-05 21:43:08 +0300

    add class

--- src/test/java/CardDeliveryServiceTest.java
+++ src/test/java/CardDeliveryServiceTest.java
@@ -1,4 +1,22 @@
+import org.junit.jupiter.api.Test;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Condition.*;
+
 public class CardDeliveryServiceTest {
+
+
+    @Test
+    void inputPositive() {
+
+        open("http://localhost:9999/");
+
+
+
+
+
+
+
+    }
 
 
 
